a3d6165ee2aca1fb3112a001486750e2e5b78512
mattansb <35330040+mattansb@users.noreply.github.com>
2021-04-25 17:40:48 +0300

    link to ggplot geoms

--- R/expected.R
+++ R/expected.R
@@ -1,10 +1,10 @@
 #' Basic mean / CI building blocks
 #'
 #' For now, these are just exported and renamed:
-#' - `geom_expected_point` is `geom_point`
-#' - `geom_CI_bar` is `geom_errorbar`
-#' - `geom_expected_line` is `geom_line`
-#' - `geom_CI_ribbon` is `geom_ribbon`
+#' - `geom_expected_point` is [geom_point()]
+#' - `geom_CI_bar` is [geom_errorbar()]
+#' - `geom_expected_line` is [geom_line()]
+#' - `geom_CI_ribbon` is [geom_ribbon()]
 #'
 #' @inheritParams ggplot2::geom_point
 #' @inheritParams ggplot2::geom_errorbar
